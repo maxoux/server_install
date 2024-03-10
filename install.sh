de73a84535e33ae3a697296d11a9cf6a5ee7e5ab
Matthieu Laizé <mat.laize@gmail.com>
2024-03-10 16:00:47 +0100

    add gpu install

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@
 # FLAG_BTOP=1
 # FLAG_NODE=1
 # FLAG_FAIL2BAN=1
-# FLAG_GPU
+# FLAG_GPU=1
+# FLAG_GPU_DOCKER_TOOLKIT=1
 
 # FLAG_CLEANUP=1
 
@@ -15,8 +16,6 @@ BASHRC_URL=https://raw.githubusercontent.com/maxoux/server_install/main/.bashrc
 WORK_DIR=$(mktemp -d)
 PREV_DIR=$(pwd)
 
-echo Working Directory: $WORK_DIR
-cd $WORK_DIR
 
 announce () {
   echo "\n\n\n"
@@ -24,6 +23,12 @@ announce () {
   echo $*;
   echo "----------------------------------------------------"
 }
+
+# Check root
+if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
+echo Working Directory: $WORK_DIR
+cd $WORK_DIR
 
 if [ -n "$FLAG_UPDATE" ]; then
   announce Updating...
@@ -79,7 +84,7 @@ if [ -n "$FLAG_NODE" ]; then
 fi
 
 # fail2ban
-if [ -n "$FLAG_BTOP" ]; then
+if [ -n "$FLAG_FAIL2BAN" ]; then
   announce Installing Fail2Ban
   apt-get install fail2ban
   systemctl start fail2ban
@@ -87,7 +92,22 @@ if [ -n "$FLAG_BTOP" ]; then
 fi
 
 # Nvidia driver
+if [ -n "$FLAG_GPU" ]; then
+  announce Install GPU drivers
+  apt-get install nvidia-driver
 
+  if [ -n "$FLAG_GPU_DOCKER_TOOLKIT" ]; then
+    announce Install GPU Docker Toolkit
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    apt-get update
+    apt-get install -y nvidia-container-toolkit
+    nvidia-ctk runtime configure --runtime=docker
+    systemctl restart docker
+  fi
+fi
 
 # Cleanup
 announce Cleanup
